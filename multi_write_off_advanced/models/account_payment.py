33050c3a78c6d37c9eba22924b4728965d459189
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-03-24 03:47:06 +0200

    multi_write_off

--- multi_write_off_advanced/models/account_payment.py
+++ multi_write_off_advanced/models/account_payment.py
@@ -149,7 +149,8 @@ class payment_register(models.TransientModel):
             pay.collection_rep_name = self.collection_rep_name
 
             print(pay.check_number, self.check_number, '888888888888888888888888888888888888888')
-        payments.action_post()
+        # payments.action_post()
+        payments.state='posted'
 
         domain = [('account_internal_type', 'in', ('receivable', 'payable')), ('reconciled', '=', False)]
         for payment, lines in zip(payments, to_reconcile):
@@ -158,6 +159,7 @@ class payment_register(models.TransientModel):
                 (payment_lines + lines)\
                     .filtered_domain([('account_id', '=', account.id), ('reconciled', '=', False)])\
                     .reconcile()
+        payments.state = 'draft'
 
         return payments
 
@@ -396,163 +398,7 @@ class account_payment(models.Model):
                     })
         return line_vals_list
 
-    # def _prepare_payment_moves(self):
-    #     if not self.post_diff_acc == 'multi':
-    #         return super(account_payment, self)._prepare_payment_moves()
-    #     all_move_vals = []
-    #     for payment in self:
-    #         company_currency = payment.company_id.currency_id
-    #         move_names = payment.move_name.split(
-    #             payment._get_move_name_transfer_separator()) if payment.move_name else None
-    #
-    #         write_off_amount = payment.payment_difference_handling == 'reconcile' and -payment.payment_difference or 0.0
-    #         if payment.payment_type in ('outbound', 'transfer'):
-    #             counterpart_amount = payment.amount
-    #             liquidity_line_account = payment.journal_id.default_debit_account_id
-    #         else:
-    #             counterpart_amount = -payment.amount
-    #             liquidity_line_account = payment.journal_id.default_credit_account_id
-    #
-    #         if payment.currency_id == company_currency:
-    #             balance = counterpart_amount
-    #             write_off_balance = write_off_amount
-    #             counterpart_amount = write_off_amount = 0.0
-    #             currency_id = False
-    #         else:
-    #             balance = payment.currency_id._convert(counterpart_amount, company_currency, payment.company_id,
-    #                                                    payment.payment_date)
-    #             write_off_balance = payment.currency_id._convert(write_off_amount, company_currency, payment.company_id,
-    #                                                              payment.payment_date)
-    #             currency_id = payment.currency_id.id
-    #
-    #         if payment.journal_id.currency_id and payment.currency_id != payment.journal_id.currency_id:
-    #             liquidity_line_currency_id = payment.journal_id.currency_id.id
-    #             liquidity_amount = company_currency._convert(
-    #                 balance, payment.journal_id.currency_id, payment.company_id, payment.payment_date)
-    #         else:
-    #             liquidity_line_currency_id = currency_id
-    #             liquidity_amount = counterpart_amount
-    #
-    #         rec_pay_line_name = ''
-    #         if payment.payment_type == 'transfer':
-    #             rec_pay_line_name = payment.name
-    #         else:
-    #             if payment.partner_type == 'customer':
-    #                 if payment.payment_type == 'inbound':
-    #                     rec_pay_line_name += _("Customer Payment")
-    #                 elif payment.payment_type == 'outbound':
-    #                     rec_pay_line_name += _("Customer Credit Note")
-    #             elif payment.partner_type == 'supplier':
-    #                 if payment.payment_type == 'inbound':
-    #                     rec_pay_line_name += _("Vendor Credit Note")
-    #                 elif payment.payment_type == 'outbound':
-    #                     rec_pay_line_name += _("Vendor Payment")
-    #             if payment.invoice_ids:
-    #                 rec_pay_line_name += ': %s' % ', '.join(payment.invoice_ids.mapped('name'))
-    #
-    #         if payment.payment_type == 'transfer':
-    #             liquidity_line_name = _('Transfer to %s') % payment.destination_journal_id.name
-    #         else:
-    #             liquidity_line_name = payment.name
-    #         move_vals = {
-    #             'date': payment.payment_date,
-    #             'ref': payment.communication,
-    #             'journal_id': payment.journal_id.id,
-    #             'currency_id': payment.journal_id.currency_id.id or payment.company_id.currency_id.id,
-    #             'partner_id': payment.partner_id.id,
-    #             'line_ids': [
-    #                 (0, 0, {
-    #                     'name': rec_pay_line_name,
-    #                     'amount_currency': counterpart_amount + write_off_amount,
-    #                     'currency_id': currency_id,
-    #                     'debit': balance + write_off_balance > 0.0 and balance + write_off_balance or 0.0,
-    #                     'credit': balance + write_off_balance < 0.0 and -balance - write_off_balance or 0.0,
-    #                     'date_maturity': payment.payment_date,
-    #                     'partner_id': payment.partner_id.id,
-    #                     'account_id': payment.destination_account_id.id,
-    #                     'payment_id': payment.id,
-    #                 }),
-    #                 (0, 0, {
-    #                     'name': liquidity_line_name,
-    #                     'amount_currency': -liquidity_amount,
-    #                     'currency_id': liquidity_line_currency_id,
-    #                     'debit': balance < 0.0 and -balance or 0.0,
-    #                     'credit': balance > 0.0 and balance or 0.0,
-    #                     'date_maturity': payment.payment_date,
-    #                     'partner_id': payment.partner_id.id,
-    #                     'account_id': liquidity_line_account.id,
-    #                     'payment_id': payment.id,
-    #                 }),
-    #             ],
-    #         }
-    #         if write_off_balance:
-    #             for woff_payment in self.writeoff_multi_acc_ids:
-    #                 write_off_amount = payment.payment_difference_handling == 'reconcile' and -woff_payment.amount or 0.0
-    #                 if payment.currency_id == company_currency:
-    #                     write_off_balance = write_off_amount
-    #                 else:
-    #                     write_off_balance = payment.currency_id._convert(write_off_amount, company_currency,
-    #                                                                      payment.company_id,
-    #                                                                      payment.payment_date)
-    #                 move_vals['line_ids'].append((0, 0, {
-    #                     'name': woff_payment.name,
-    #                     'amount_currency': -write_off_amount,
-    #                     'currency_id': currency_id,
-    #                     'debit': write_off_balance < 0.0 and -write_off_balance or 0.0,
-    #                     'credit': write_off_balance > 0.0 and write_off_balance or 0.0,
-    #                     'date_maturity': payment.payment_date,
-    #                     'partner_id': payment.partner_id.id,
-    #                     'account_id': woff_payment.writeoff_account_id.id,
-    #                     'payment_id': payment.id,
-    #                 }))
-    #         if move_names:
-    #             move_vals['name'] = move_names[0]
-    #
-    #         all_move_vals.append(move_vals)
-    #
-    #         if payment.payment_type == 'transfer':
-    #
-    #             if payment.destination_journal_id.currency_id:
-    #                 transfer_amount = payment.currency_id._convert(counterpart_amount,
-    #                                                                payment.destination_journal_id.currency_id,
-    #                                                                payment.company_id, payment.payment_date)
-    #             else:
-    #                 transfer_amount = 0.0
-    #
-    #             transfer_move_vals = {
-    #                 'date': payment.payment_date,
-    #                 'ref': payment.communication,
-    #                 'partner_id': payment.partner_id.id,
-    #                 'journal_id': payment.destination_journal_id.id,
-    #                 'line_ids': [
-    #                     (0, 0, {
-    #                         'name': payment.name,
-    #                         'amount_currency': -counterpart_amount,
-    #                         'currency_id': currency_id,
-    #                         'debit': balance < 0.0 and -balance or 0.0,
-    #                         'credit': balance > 0.0 and balance or 0.0,
-    #                         'date_maturity': payment.payment_date,
-    #                         'partner_id': payment.partner_id.id,
-    #                         'account_id': payment.company_id.transfer_account_id.id,
-    #                         'payment_id': payment.id,
-    #                     }),
-    #                     (0, 0, {
-    #                         'name': _('Transfer from %s') % payment.journal_id.name,
-    #                         'amount_currency': transfer_amount,
-    #                         'currency_id': payment.destination_journal_id.currency_id.id,
-    #                         'debit': balance > 0.0 and balance or 0.0,
-    #                         'credit': balance < 0.0 and -balance or 0.0,
-    #                         'date_maturity': payment.payment_date,
-    #                         'partner_id': payment.partner_id.id,
-    #                         'account_id': payment.destination_journal_id.default_credit_account_id.id,
-    #                         'payment_id': payment.id,
-    #                     }),
-    #                 ],
-    #             }
-    #             if move_names and len(move_names) == 2:
-    #                 transfer_move_vals['name'] = move_names[1]
-    #             all_move_vals.append(transfer_move_vals)
-    #     return all_move_vals
+
 
 
 class writeoff_accounts(models.Model):
