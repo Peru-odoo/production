8aaa6bdcd34f45c8a2ae041bd0e32587c3ea7a2e
surgi-tech <it.dep@surgitech.net>
2021-03-24 13:39:29 +0200

    surgi_in_out_inventory_restriction

--- surgi_in_out_inventory_restriction/models/stock_picking_changes.py
+++ surgi_in_out_inventory_restriction/models/stock_picking_changes.py
@@ -15,19 +15,16 @@ class stock_picking_inherit(models.Model):
 
     def is_valid_show(self):
         pick_type = self.picking_type_id.code
-        if pick_type == 'internal':
-            self.is_valid = True
+        # if pick_type == 'internal':
+        #     self.is_valid = True
 
-        elif pick_type == 'incoming' and self.env.user.has_group('surgi_in_out_inventory_restriction.validate_in_group'):
+        if pick_type == 'incoming' and self.env.user.has_group('surgi_in_out_inventory_restriction.validate_in_group'):
             self.is_valid = True
 
         elif pick_type == 'outgoing' and self.env.user.has_group('surgi_in_out_inventory_restriction.validate_out_group'):
             self.is_valid = True
 
-        elif pick_type == 'internal' and source_location_is_freeze == False:
-            self.is_valid = True
-
-        elif pick_type == 'internal' and destination_location_is_freeze == False:
+        elif pick_type == 'internal' and self.location_id.operation_location_freeze == False and self.location_dest_id.operation_location_freeze == False:
             self.is_valid = True
 
         else:
