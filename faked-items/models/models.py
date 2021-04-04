f4ad8cdae6b2471945818e5717aed5ef378c4c15
surgi-tech <it.dep@surgitech.net>
2021-04-04 07:11:06 +0200

    faked-items - stock_picking_back2draft

---
+++ faked-items/models/models.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import json
+from pprint import pprint
+from datetime import datetime, timedelta
+
+from odoo import models, fields, api
+
+
+# class faked-items(models.Model):
+#     _name = 'faked-items.faked-items'
+#     _description = 'faked-items.faked-items'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+class stock_biking_inherts(models.Model):
+    _inherit="stock.picking"
+    fakeditem=fields.One2many(comodel_name="stock.biking.faked.item",inverse_name="stock_bikeid")
+    pass
+
+
+class stock_biking_faked_item(models.Model):
+    _name='stock.biking.faked.item'
+    name=fields.Char("Lot Name")
+    serial = fields.Char("Lot Serial")
+    product = fields.Integer()
+    expirartion=fields.Char("Expiration Date")
+    quantity=fields.Integer("Quantity")
+    stock_bikeid=fields.Many2one(comodel_name="stock.picking",relation="stock.picking")
+
+    @api.model
+    def get_scanned_items(self,active_id):
+        print("ff")
+        olditems={}
+        #activeid=self.active_id
+        items=self.env["stock.biking.faked.item"].search([('stock_bikeid', '=', int(active_id))])
+        tquantity=0
+        for i in items:
+           olditems[i.serial]={'id':i.id,
+                               "serial":i.serial,
+                               "quantity":i.quantity,
+                               'name':i.name,
+                               'product':i.product.id,
+                               'stock_bikeid':i.stock_bikeid.id,
+                               'expirartion':i.expirartion}
+           tquantity+=i.quantity
+        returnData={"olditems":olditems,'tquantity':tquantity}
+        return json.dumps(returnData, ensure_ascii=False)
+        pass
+    @api.model
+    def savescanned_items(self,active_id,newitems,updateitems):
+        print("x")
+        stockpick=self.env["stock.picking"].search([('id', '=', int(active_id))])
+        for i in updateitems.values():
+            item = self.env["stock.biking.faked.item"].search([('id', '=', int(i['id']))])
+            item.quantity=i['quantity']
+            pass
+        if len(newitems):
+            for i in newitems.values():
+                product=self.env['stock.production.lot'].search([('name', '=', str(i['serial'])),("company_id","=",stockpick.company_id.id)])
+                i['product']=product.id
+                self.env['stock.biking.faked.item'].create(i)
+            pass
+        pass
+    pass
