2b5a1470b1b6db23f91b605b1bfa82b19bddf9a0
mostafa-nassar <mostafa.nassar@outlook.com>
2021-05-25 09:14:16 +0200

    disable compute_date_check_number

--- surgi_operation/models/stock_quant.py
+++ surgi_operation/models/stock_quant.py
@@ -3,10 +3,6 @@ from odoo import fields, models, api, exceptions
 
 class stock_quant_inherit_wizard(models.Model):
     _inherit = 'stock.quant'
-    @api.model
-    def create_so_location(self):
-        pass
-
 
     @api.depends('location_id.warehouse_id.warehouse_users')
     def _get_wh_user(self):
