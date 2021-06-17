c839797d647219fbef3ec6040327b7af5cf93ec3
mostafa-nassar <mostafa.nassar@outlook.com>
2021-06-17 12:06:21 +0200

    add sales balance field

--- surgi_analytic_account/models/models.py
+++ surgi_analytic_account/models/models.py
@@ -94,6 +94,15 @@ class AccountMoveLineInherit(models.Model):
     analytic_account_group_parent_id = fields.Many2one(comodel_name='account.analytic.group',
                                                        related='analytic_account_group_id.parent_id',
                                                        string='AC. Group Parent', readonly=True, store=True)
+    sales_balance = fields.Monetary(string='Sales Balance', store=True,
+                              currency_field='company_currency_id',
+                              compute='_compute_sales_balance',
+                              help="Technical field holding the credit - debit in order to open meaningful graph views from reports")
+
+    @api.depends('debit', 'credit')
+    def _compute_sales_balance(self):
+        for line in self:
+            line.sales_balance = line.debit - line.credit
 
     # def cron_all_account_move(self):
     #     for rec in self.search([]):
