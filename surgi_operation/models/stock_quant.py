ed4751535f549352b00fe34fb6cc770db5cb36d3
osama-ahmed-ezzat <osama.surgitech@gmail.com>
2021-07-15 14:29:23 +0200

    surgi_invice_print_surgi_operation

--- surgi_operation/models/stock_quant.py
+++ surgi_operation/models/stock_quant.py
@@ -1,8 +1,28 @@
 from odoo import fields, models, api, exceptions
 
 
+
+
+
+
+
+
+
+
 class stock_quant_inherit_wizard(models.Model):
     _inherit = 'stock.quant'
+    # prod_femorel = fields.Boolean(string='Femoral')
+    # prod_insert_qu = fields.Boolean(string='Insert')
+    # prod_tibial = fields.Boolean(string='Tibial')
+    # prod_cement = fields.Boolean(string='Cement')
+    # prod_replacement = fields.Boolean(string='الاستبدالات')
+    # prod_accessories = fields.Char(string='مشتملات')
+    #
+    #
+    # quant_id = fields.Char( readonly=True, compute='get_operation_locationss_quant',string='product')
+    # operation_id=fields.Many2one('operation.operation',string="Operation", readonly=True)
+    #
+
 
     @api.depends('location_id.warehouse_id.warehouse_users')
     def _get_wh_user(self):
@@ -30,7 +50,6 @@ class stock_hanged_quant_inherit(models.Model):
     _name = 'hanged.stock.quant'
     _inherit = 'stock.quant'
 
-    quant_id = fields.Many2one('stock.quant', readonly=True)
     invoice_id = fields.Many2one(comodel_name='account.move', string='Invoice', readonly=True)
     operation_id=fields.Many2one('operation.operation',string="Operation", readonly=True)
 
@@ -57,3 +76,6 @@ class stock_hanged_quant_inherit(models.Model):
         result['views'] = [(res and res.id or False, 'form')]
         result['target'] = 'new'
         return result
+
+
+
