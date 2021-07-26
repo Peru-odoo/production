04af3b5da10b494fbb567380654c8c30ff606c5d
Ahmed <ahmed887463@gmail.com>
2021-07-26 23:29:21 +0200

    Update invoice.py

--- surgi_invoice_print/models/invoice.py
+++ surgi_invoice_print/models/invoice.py
@@ -33,8 +33,9 @@ class AccountMoveInherit(models.Model):
             total = 0.0
             for line in rec.printinvoicetoline:
                 total += line.total
-            #if rec.amount_total == total:    
-            if rec.amount_total-total<1 and rec.amount_total-total>-1:
+            #if rec.amount_total == total: 
+            #rec.is_equal_total = (rec.amount_total-total>= -1 and rec.amount_total-total<=1 )
+            if (rec.amount_total-total>= -1 and rec.amount_total-total<=1 ):
                 rec.is_equal_total = True
                 rec.is_equal_total2 = True
 
