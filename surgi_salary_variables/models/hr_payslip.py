859f0aa46e8f65ba603656616a64af980157338e
mostafa-nassar <mostafa.nassar@outlook.com>
2021-09-30 08:20:51 +0200

    surgi_salary_variables\models\hr_payslip.py

--- surgi_salary_variables/models/hr_payslip.py
+++ surgi_salary_variables/models/hr_payslip.py
@@ -4,17 +4,46 @@ from odoo import models, fields, api
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    @api.model
-    def create(self, vals):
-        res = super(HrPayslip, self).create(vals)
-        variable_allowance = self.env['hr.variable.allowance.request'].search(
-            [('employee_id', '=', res.employee_id.id),
-             ('date', '>=', res.date_from),
-             ('date', '<=', res.date_to),
-             ('state','=','confirmed')
-             ])
+    # @api.model
+    # def create(self, vals):
+    #     res = super(HrPayslip, self).create(vals)
+    #     variable_allowance = self.env['hr.variable.allowance.request'].search(
+    #         [('employee_id', '=', res.employee_id.id),
+    #          ('date', '>=', res.date_from),
+    #          ('date', '<=', res.date_to),
+    #          ('state','=','confirmed')
+    #          ])
+    #     print(variable_allowance,'LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL')
+    #     data = {}
+    #     for it in variable_allowance:
+    #         tmp = data.get(it.rule_id.code, {})
+    #         tmp = tmp.get('amount', 0) if tmp else 0
+    #         data.update({
+    #             it.rule_id.code: {
+    #                 'amount': it.amount + tmp,
+    #                 'input_type_id': it.rule_id.payslip_input_type_id.id
+    #             }
+    #         })
+    #
+    #     for key in data:
+    #         self.env['hr.payslip.input'].create({
+    #             'input_type_id': data[key].get('input_type_id'),
+    #             'amount': data[key].get('amount'),
+    #             'payslip_id': res.id
+    #         })
+    #     print(data,'KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK22222222222222222')
+    #
+    #     return res
 
+    def compute_sheet(self):
+        variable_allowance = self.env['hr.variable.allowance.request'].search(
+            [('employee_id', '=', self.employee_id.id),
+             ('date', '>=', self.date_from),
+             ('date', '<=', self.date_to),
+             ('state', '=', 'confirmed')
+             ])
         data = {}
+
         for it in variable_allowance:
             tmp = data.get(it.rule_id.code, {})
             tmp = tmp.get('amount', 0) if tmp else 0
@@ -29,7 +58,7 @@ class HrPayslip(models.Model):
             self.env['hr.payslip.input'].create({
                 'input_type_id': data[key].get('input_type_id'),
                 'amount': data[key].get('amount'),
-                'payslip_id': res.id
+                'payslip_id': self.id
             })
-
+        res = super(HrPayslip, self).compute_sheet()
         return res
