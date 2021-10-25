815d79dfb79fc42e499ee84c32a88c3b57ec9c22
Ahmed <ahmed887463@gmail.com>
2021-10-26 00:15:54 +0200

    Update hr_attendance_sheet.py

--- surgi_attendance_sheet/models/hr_attendance_sheet.py
+++ surgi_attendance_sheet/models/hr_attendance_sheet.py
@@ -299,10 +299,6 @@ class AttendanceSheet(models.Model):
                             if shiftselected:
                                 print("break")
                                 break
-                    if not shiftselected:
-                        work_intervals = calendar_id.att_get_work_intervals(
-                            day_start,
-                            day_end, tz)
 
 
 
@@ -315,6 +311,7 @@ class AttendanceSheet(models.Model):
                         day_start,
                         day_end, tz)
                 if not attendance_intervals:
+
                     attendance_intervals = self.get_attendance_intervals(emp,
                                                                          day_start,
                                                                          day_end,
