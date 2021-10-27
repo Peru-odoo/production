56be34bb6334572a86155fe48425ce1afa8c4171
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-10-27 14:27:46 +0200

    test

--- surgi_regularity/__manifest__.py
+++ surgi_regularity/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'security/reg_security.xml'
+        'security/reg_security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
