7e6f0b520fd46240e145dc2af04f425841858148
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-10-28 13:23:50 +0200

    test

--- surgi_regularity/models/models.py
+++ surgi_regularity/models/models.py
@@ -6,22 +6,22 @@ from dateutil.relativedelta import relativedelta
 from odoo.exceptions import ValidationError, UserError
 
 
-# class regularity_registration_line(models.Model):
-#     _name = 'registration.line'
-#
-#
-#
-#
-#     country_of_orgin = fields.Many2many(comodel_name="res.country",string="Country of Orgin")
-#     product_registration_number = fields.Char("registration Number")
-#     strlize_method = fields.Char(string="Sterilization method")
-#     leagal_munfacter=fields.Char("legal Manufacturer")
-#     acual_munfacter = fields.Many2many('acual.munfacter',"name",string="Actual Manufacturer")
-#     releas_date = fields.Date(String="Release Date")
-#     expiry_date = fields.Date(string="Expiry date")
-#     attachment_product = fields.Binary( string="Attachment")
-#     product_forms = fields.One2many('product.template','registration_line' ,string='المنتج')
-#
+class regularity_registration_line(models.Model):
+    _name = 'registration.line'
+
+
+
+    name = fields.Char("Registration Name")
+    country_of_orgin = fields.Many2many(comodel_name="res.country",string="Country of Orgin")
+    product_registration_number = fields.Char("registration Number")
+    strlize_method = fields.Char(string="Sterilization method")
+    leagal_munfacter=fields.Char("legal Manufacturer")
+    acual_munfacter = fields.Many2many('acual.munfacter',"name",string="Actual Manufacturer")
+    releas_date = fields.Date(String="Release Date")
+    expiry_date = fields.Date(string="Expiry date")
+    attachment_product = fields.Binary( string="Attachment")
+    product_forms = fields.One2many('product.template','registration_line')
+
 
 
 
@@ -35,6 +35,10 @@ class product_template_changes(models.Model):
 
     ], string="Product Type")
 
+
+
+
+
     strlize_field=fields.Boolean(string="Sterile")
     item_num_ref = fields.Char("Internal Reference")
     label_ref_num = fields.Char("Label Reference")
@@ -46,7 +50,7 @@ class product_template_changes(models.Model):
     acual_munfacter = fields.Many2many('acual.munfacter',"name",string="Actual Manufacturer")
     country_of_orgin = fields.Many2many(comodel_name="res.country",string="Country of Orgin")
 
-    # registration_line = fields.Many2one("registration.line")
+    registration_line = fields.Many2one("registration.line","registration line")
 
 
 
