1af88964378d28bf2bbe8ba385d00e10402aa5a0
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-11-09 09:01:04 +0200

    Update __manifest__.py

--- surgi_warehouse_users/__manifest__.py
+++ surgi_warehouse_users/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['stock','stock_account','surgi_product_template','sale'],
 
     # always loaded
     'data': [
