05ce49ea8b5c733269fe932b29f8c863d854c669
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-11-09 09:13:52 +0200

    test

--- surgi_warehouse_users/models/models.py
+++ surgi_warehouse_users/models/models.py
@@ -18,3 +18,19 @@ class StockPickingInherit(models.Model):
 
     warehouse_id_user=fields.Many2one(
         related="picking_type_id.warehouse_id")
+
+
+class stock_quant_inherit_wizard(models.Model):
+    _inherit = 'stock.quant'
+
+
+    is_wh_user = fields.Boolean(default=False, compute=_get_wh_user,store=True)
+
+    @api.depends('location_id.warehouse_id.warehouse_users')
+    def _get_wh_user(self):
+        for obj in self:
+            for user in obj.location_id.warehouse_id.warehouse_users:
+                if(self.env.user.id == user.id):
+                    obj.is_wh_user=True
+                    break
+                print ("WH result: ",obj.is_wh_user)
