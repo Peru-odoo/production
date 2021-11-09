cb5c330c18c249c4db4d785a22a586c5de899734
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-11-09 09:44:28 +0200

    test

--- surgi_inventory_changes/models/stock_warehouse_changes.py
+++ surgi_inventory_changes/models/stock_warehouse_changes.py
@@ -4,6 +4,7 @@ from odoo.exceptions import Warning
 
 class StockWarehouseInherit(models.Model):
     _inherit = 'stock.warehouse'
+    warehouse_users = fields.Many2many('res.users', string="Users")
 
     warehouse_managers_id = fields.Many2one('res.users', string="Manager")
     
@@ -11,6 +12,10 @@ class StockWarehouseInherit(models.Model):
 
     stock_branches = fields.Many2one('branch.location',string='Branch',store=True)
 # ================= A.Salama ==================
+class stock_picking_type_changes(models.Model):
+    _inherit = 'stock.picking.type'
+    # add field to relate with many2many field in stock warehuse
+    warehouse_users = fields.Many2many(related="warehouse_id.warehouse_users",comodel_name='res.users', string="Users")
 
 
 class branches_location_wharehouse(models.Model):
