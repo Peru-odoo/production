469c8283b8f99f65b9b190da962609dc9ed7ef77
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-11-09 09:37:39 +0200

    Update __manifest__.py

--- surgi_inventory_changes/__manifest__.py
+++ surgi_inventory_changes/__manifest__.py
@@ -13,7 +13,7 @@ Warehouse modifications
         'views/stock_picking_type_changes_view.xml',
         'views/stock_warehouse_view_changes.xml',
         'views/stock_location_changes_view.xml',
-        'views/stock_warehouse_changes_view.xml',
+        # 'views/stock_warehouse_changes_view.xml',
         'views/sale.order.xml',
         'views/stock_quant_change_view.xml',
         'views/stock_move_line_changes.xml',
