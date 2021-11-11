002bb7defbf2c74b3999a7abc89c09cc236b062d
Osama-Ahmed-Ezzat <67324627+Osama-Ahmed-Ezzat@users.noreply.github.com>
2021-11-11 14:48:41 +0200

    test

--- surgi_inventory_changes/models/managers_line.py
+++ surgi_inventory_changes/models/managers_line.py
@@ -6,3 +6,22 @@ class manager_line(models.Model):
     manager_id = fields.Many2one('res.users')
 
     warehouse_id = fields.Many2one('stock.warehouse')
+
+
+class Stock_quent_mangerInherit(models.Model):
+    _inherit = 'stock.quant'
+
+    @api.model
+    def button_stock_onhand_action(self):
+        # ctx = dict(
+        #     create=False,
+        # )
+        value = {
+            'name': 'Product Line Report',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'stock.quant',
+            'type': 'ir.actions.act_window',
+            'domain': [('on_hand', '=', True),('product_id.product_line_id.warehouse_users','=',uid)]
+        }
+        return value
